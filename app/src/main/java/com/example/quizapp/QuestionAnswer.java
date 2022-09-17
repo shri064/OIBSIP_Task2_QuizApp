d8d8fef3bcca893ab1dc996835b7fbf124e357cf
waykuleshri@gmail.com <waykuleshri@gmail.com>
2022-09-17 15:20:38 +0530

    Sample Commit

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,44 @@
+package com.example.quizapp;
+
+class QuestionAnswer {
+
+    public static String[] question ={
+            "Which company owns the android?",
+            "Full form of OOP",
+            "Which one is not the programming language?",
+            "Who is known as father of java?",
+            "Tim Cook is a CEO of which company?",
+            "Which year OASIS founded?",
+            "Who invented computers?",
+            "Which is not high programming language?",
+            "Which IIT have developed AI based disease detector",
+            "When Programmer's Day celebrate?"
+    };
+
+    public static String[][] choices = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Open Oriented Programming","Object Oriented Programming","Open Object Programming","None of the above"},
+            {"Java","C++","Notepad","Python"},
+            {"James Gosling","Guido van Rossum","Bjarne Stroustrup","Tim Berners-Lee"},
+            {"Google","Apple","Nokia","Samsung"},
+            {"2001","1993","1998","2005"},
+            {"John Herschel","George Peacock","Edward Ryan","Charles Babbage"},
+            {"Ruby","Java","C++","Python"},
+            {"IIT-Hyderabad","IIT-Madras","IIT-Delhi","IIT-Mumbai"},
+            {"2 Oct","13 Sep","14 April","14 May"}
+    };
+
+    public static String[] correctAnswers = {
+            "Google",
+            "Object Oriented Programming",
+            "Notepad",
+            "James Gosling",
+            "Apple",
+            "1993",
+            "Charles Babbage",
+            "Ruby",
+            "IIT-Delhi",
+            "13 Sep"
+    };
+
+}
